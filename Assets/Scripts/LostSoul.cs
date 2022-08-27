1e36675c4c04116d35787b4c1522d4549c1bb1d1
eon704 <eon704@gmail.com>
2022-08-26 21:53:39 -0400

    Added accepting new souls and light radius increase

---
+++ Assets/Scripts/LostSoul.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class LostSoul : MonoBehaviour {
+
+  private void OnTriggerEnter2D(Collider2D other) {
+    if (other.CompareTag("Player")) {
+      other.GetComponent<Player>().AcceptNewGroupMember();
+      // col.enabled = false;
+      Destroy(this.gameObject);
+    }
+  }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using Interfaces;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Player : MonoBehaviour {
 
-  [SerializeField] private int health;
   [SerializeField] private float speed = 1f;
   [SerializeField] private int damage = 1;
   [SerializeField] private float attackRange = 2f;
   [SerializeField] private float attackCooldown = 4f;
 
   private Rigidbody2D rb2d;
+  private Light2D soulLight;
   private IDamageable target;
   private bool isOnAttackCooldown;
 
   private readonly RaycastHit2D[] targetsBuffer = new RaycastHit2D[20];
 
+  private int Health {
+    get => this.groupMembersCount;
+    set {
+      this.groupMembersCount = value;
+      this.soulLight.pointLightOuterRadius = 6 + this.groupMembersCount;
+    }
+  }
+
+  private int groupMembersCount = 1;
+
+  #region Unity Callbacks
   private void Start() {
     this.rb2d = this.GetComponent<Rigidbody2D>();
+    this.soulLight = this.GetComponentInChildren<Light2D>();
   }
 
   private void Update() {
@@ -40,16 +53,26 @@ public class Player : MonoBehaviour {
     UnityEditor.Handles.DrawWireDisc(this.transform.position, Vector3.forward, this.attackRange);
   }
 
-  // Public methods
-  public void TakeDamage(int amount) {
-    this.health -= amount;
+  #endregion
 
-    if (this.health <= 0) {
+  #region Public methods
+
+  public void TakeDamage(int amount) {
+    this.Health -= amount;
+
+    if (this.Health <= 0) {
       Debug.Log("You have died");
     }
   }
 
-  // Private methods
+  public void AcceptNewGroupMember() {
+    this.Health++;
+  }
+
+  #endregion
+
+  #region private methods
+
   private void StartAttack() {
     this.GetTarget();
     if (this.target != null && !this.isOnAttackCooldown) {
@@ -87,5 +110,6 @@ public class Player : MonoBehaviour {
     int randomIndex = Random.Range(0, targetsFound);
     this.target = this.targetsBuffer[randomIndex].transform.GetComponent<IDamageable>();
   }
+  #endregion
 }
 
