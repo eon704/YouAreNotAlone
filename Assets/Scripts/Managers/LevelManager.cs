961e8c6754433f60c01feb6b858a81c0b1d26771
eon704 <eon704@gmail.com>
2022-08-27 00:21:45 -0400

    Reorganized Scripts

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine.SceneManagement;
+
+namespace Managers {
+  public class SceneManager {
+    public static void LoadScene() {
+      UnityEngine.SceneManagement.SceneManager.LoadScene("")
+    }
+  }
+}
