58c4a584bae6bfd66659894e286812e96c3680d5
eon704 <eon704@gmail.com>
2022-08-27 23:47:32 -0400

    Added minor failsafe

--- Assets/Scripts/Utilities/GlobalLights.cs
+++ Assets/Scripts/Utilities/GlobalLights.cs
@@ -5,6 +5,10 @@ public class GlobalLights : MonoBehaviour {
   [SerializeField] private bool isEnabled;
 
   void Awake() {
+    #if UNITY_EDITOR
     this.gameObject.SetActive(this.isEnabled);
+    #else
+    this.gameObject.SetActive(false);
+    #endif
   }
 }
